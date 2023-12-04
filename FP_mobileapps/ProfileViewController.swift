9e73e3935a25e82ad5bb58126d13eb6a353c4e83
Richelle Plummer <rplummer2020@fau.edu>
2023-12-04 16:48:25 -0500

    can choose an avatar for profile pic

--- FP_mobileapps/ProfileViewController.swift
+++ FP_mobileapps/ProfileViewController.swift
@@ -15,9 +15,36 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var fullNameLabel: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
     
+    let avatarImages = [
+        "avatar1",
+        "avatar2",
+        "avatar3",
+        "avatar4",
+        "avatar5",
+        "avatar6",
+        "avatar7",
+        "avatar8",
+        "avatar9",
+        "avatar10",
+        "avatar11",
+        "avatar12",
+        "avatar13",
+        "avatar14",
+        "avatar15"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadUserData()
+        
+        let tapGesture = UITapGestureRecognizer(target:self, action: #selector(profilePicTapped))
+        profilePic.addGestureRecognizer(tapGesture)
+        profilePic.isUserInteractionEnabled = true
+        
+        // Load the selected avatar from UserDefaults
+        if let selectedAvatar = UserDefaults.standard.string(forKey: "selectedAvatar") {
+                   profilePic.image = UIImage(named: selectedAvatar)
+               }
     }
     
   
@@ -39,7 +66,42 @@ class ProfileViewController: UIViewController {
                
             } else {
                 showAlert(description: "User not Found")
-            }    }
+            }
+        
+    }
+    
+    @objc func profilePicTapped(){
+        let alertController = UIAlertController(title: "Select Avatar", message: nil, preferredStyle: .actionSheet)
+
+                // Add action for each avatar image
+                for avatarName in avatarImages {
+                    if let image = UIImage(named: avatarName) {
+                        let action = UIAlertAction(title: "", style: .default) { [weak self] _ in
+                            // Set the selected avatar to the profile picture
+                            self?.profilePic.image = image
+
+                            // Save the selected avatar name to UserDefaults
+                            UserDefaults.standard.set(avatarName, forKey: "selectedAvatar")
+                            
+                            // Synchronize UserDefaults to ensure data is saved immediately
+                            UserDefaults.standard.synchronize()
+                        }
+
+                        // Add a preview image to the action
+                        action.setValue(image.withRenderingMode(.alwaysOriginal), forKey: "image")
+
+                        alertController.addAction(action)
+                    }
+                }
+
+                // Add a cancel action
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                alertController.addAction(cancelAction)
+
+                // Present the alert controller
+                present(alertController, animated: true, completion: nil)
+            }
+    
     
     @IBAction func onLogOutTapped(_ sender: Any) {
         showConfirmLogoutAlert()

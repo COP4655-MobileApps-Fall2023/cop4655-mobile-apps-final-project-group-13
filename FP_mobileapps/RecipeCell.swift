2f17f8bfd5eff843ae3c8011bfe765841bb40537
Dayaan Mazhar <dmazhar2019@fau.edu>
2023-12-05 08:15:46 -0500

    Updated stream screen UI and backend logic

--- FP_mobileapps/RecipeCell.swift
+++ FP_mobileapps/RecipeCell.swift
@@ -10,14 +10,13 @@ import Nuke
 
 class RecipeCell: UITableViewCell {
     
-    
     @IBOutlet weak var recipeName: UILabel!
     @IBOutlet weak var recipesImage: UIImageView!
-    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        setupUI()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -26,37 +25,67 @@ class RecipeCell: UITableViewCell {
     
     // Configures the cell's UI for the given recipe.
     func configure(with recipes: Recipes) {
-        // Check if strCategory is not empty
+        // Check if strMeal is not empty
         guard !recipes.strMeal.isEmpty else {
-            // Handle the case where strCategory is empty
-            print("Error: Recipe category is empty")
+            // Handle the case where strMeal is empty
+            print("Error: Recipe name is empty")
             return
         }
 
         // Safely unwrap title
         if let unwrapTitle = recipeName {
             unwrapTitle.text = recipes.strMeal
+
+            // Apply text shadow
+            unwrapTitle.layer.shadowColor = UIColor.black.cgColor
+            unwrapTitle.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+            unwrapTitle.layer.shadowOpacity = 0.5
+            unwrapTitle.layer.shadowRadius = 1.0
         } else {
             // Handle the case where title is nil
             print("Error: Title label is nil")
         }
 
-        // Safely unwrap recipeImage
+        // Safely unwrap recipesImage
         if let unwrapRecipeImage = recipesImage {
-            // Convert recipe.id to String before concatenating
+            // Convert strMealThumb to String before concatenating
             let imageUrlStr = recipes.strMealThumb
             
             // Convert the URL string to URL
             if let imageUrl = URL(string: imageUrlStr) {
-                // Load image async via Nuke library image loading helper method
+                // Load image asynchronously using Nuke library's image loading helper method
                 Nuke.loadImage(with: imageUrl, into: unwrapRecipeImage)
             } else {
                 // Handle the case where the URL cannot be created
                 print("Error: Invalid URL")
             }
         } else {
-            // Handle the case where recipeImage is nil
+            // Handle the case where recipesImage is nil
             print("Error: Recipe image view is nil")
         }
+    }
+    
+    // Set up the UI for the cell
+    private func setupUI() {
+        // Round corners of the entire cell
+        contentView.layer.cornerRadius = 12
+        contentView.layer.masksToBounds = true
+
+        // Add gradient overlay to darken the image
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.8, 1.0] // Adjust the gradient stops as needed
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        gradientLayer.frame = contentView.bounds
+
+        contentView.layer.addSublayer(gradientLayer)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        // Add spacing at the bottom
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 24, right: 0))
     }
 }
